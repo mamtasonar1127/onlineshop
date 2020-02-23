49b4aa8b6c17c3963d662fb3c87e30797476fe80
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-02-22 22:07:32 -0500

    created server request and response function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
+var port = process.env.PORT || 3000;
+
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); //getting data from datajson file
 
 const laptopData = JSON.parse(json);
@@ -8,25 +12,11 @@ console.log(laptopData);
 //Creating a server
 const server = http.createServer((req, res) => {
     
-    const pathName = url.parse(req.url, true).pathname;
-    const id = url.parse(req.url, true).query.id;
+    res.end('Hello');
     
-    // Product Overview
-    if (pathName === '/products' || pathName === '/') {
-        res.writeHead(200, { 'Content-type': 'text/html'});
-        
-        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
-            let overviewOutput = data;
-            
-            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
-            
-                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
-                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput); 
-                
-                res.end(overviewOutput);
-            });
-        });
-        
-        
-    }
+    
+});
+
+server.listen(3000, () => {
+    console.log('Listening for requests now');
 });
