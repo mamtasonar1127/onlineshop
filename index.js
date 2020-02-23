061598a0912c2cca8cca10be4fd2ff0977c0d139
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-02-22 21:43:37 -0500

    create server with product view

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); //getting 
 
 const laptopData = JSON.parse(json);
 console.log(laptopData);
+
+//Creating a server
+const server = http.createServer((req, res) => {
+    
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+    
+    // Product Overview
+    if (pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+            
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+            
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput); 
+                
+                res.end(overviewOutput);
+            });
+        });
+        
+        
+    }
+});
